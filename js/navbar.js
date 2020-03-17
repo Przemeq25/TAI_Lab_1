2ea989068f03a6823ce353aae095c3e6f5269c82
Przemysław Cichoń <przemekcichon97@gmail.com>
2020-03-17 23:16:05 +0100

    changed navbar to innerHTML

---
+++ js/navbar.js
@@ -0,0 +1,3 @@
+let nav = '<h1>LOGO2</h1><nav><ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li><a class="nav-link active" href="quiz.html">Quiz</a></li><li><a class="nav-link active" href="contact.html">Kontakt</a></li></ul></nav>';
+let navbar = document.querySelector('.navigation');
+navbar.innerHTML = nav;
